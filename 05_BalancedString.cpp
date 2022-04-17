580b7a9d3173d64d1f2273b47f3a768ba1cfe2be
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-18 00:13:39 +0530

    Wrong solution

---
+++ 05_BalancedString.cpp
@@ -0,0 +1,58 @@
+// https://leetcode.com/problems/split-a-string-in-balanced-strings/
+
+// Balanced strings are those that have an equal quantity of
+// 'L' and 'R' characters.
+
+// Given a balanced string s, split it in the maximum amount
+// of balanced strings.
+
+// Return the maximum amount of split balanced strings.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int balancedStringSplit(string s)
+    {
+        // No brainer - Wrong solution "RLRRRLLRLL" 
+        int count = 0;
+        int cL = 0, cR = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == 'L')
+            {
+                if (cR && cR == cL)
+                {
+                    count++;
+                    cR = cL = 0;
+                }
+                else
+                {
+                    cL++;
+                }
+            }
+            else
+            {
+                if (cL && cL == cR)
+                {
+                    count++;
+                    cR = cL = 0;
+                }
+                else
+                {
+                    cR++;
+                }
+            }
+        }
+        return count + 1;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
