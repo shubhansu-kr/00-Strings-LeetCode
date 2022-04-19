9e02e653d431f5950382978be444907bed3ac54d
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-19 20:47:41 +0530

    Find the first palindrome string

---
+++ 15_FirstPalindrome.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string firstPalindrome(vector<string> &words)
+    {
+        for (int i = 0; i < words.size(); i++)
+        {
+            int n = words[i].length();
+            int flag = 1;
+            for (int j = 0; j < n / 2; j++)
+            {
+                if (words[i][j] != words[i][n - 1 - j])
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                return words[i];
+            }
+        }
+        return "";
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
