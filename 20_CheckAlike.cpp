3e9878d6682eae22227b839a31ca16d4bf6d1d5a
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-20 21:24:37 +0530

    Solution 1

--- 20_CheckAlike.cpp
+++ 20_CheckAlike.cpp
@@ -25,6 +25,37 @@ public:
     bool halvesAreAlike(string s)
     {
         int n = s.length() / 2;
+        int counti = 0, countj = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (isVovel(s[i]))
+            {
+                if (i < n)
+                {
+                    counti++;
+                }
+                else
+                {
+                    countj++;
+                }
+            }
+        }
+        return counti == countj;
+    }
+};
+
+class Solution
+{
+public:
+    bool isVovel(char c)
+    {
+        return (c == 'a' || c == 'u' || c == 'o' || c == 'i' ||
+                c == 'e' || c == 'A' || c == 'O' || c == 'I' || c == 'E' ||
+                c == 'U');
+    }
+    bool halvesAreAlike(string s)
+    {
+        int n = s.length() / 2;
         return count_if(s.begin(), s.begin() + n, isVovel) == count_if(s.begin() + n, s.end(), isVovel);
     }
 };
