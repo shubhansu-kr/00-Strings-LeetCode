ea1f548ecedc239f8bad9d05cf97b2e68b0078ce
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-21 20:08:05 +0530

    Find the destination city

---
+++ 23_DestinationCity.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/destination-city/
+
+// You are given the array paths, where paths[i] = [cityAi, cityBi]
+// means there exists a direct path going from cityAi to cityBi.
+// Return the destination city, that is, the city without any path
+// outgoing to another city.
+
+// It is guaranteed that the graph of paths forms a line without
+// any loop, therefore, there will be exactly one destination city.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string destCity(vector<vector<string>> &paths)
+    {
+        // Both Destionation and Start will have a frequency 1.
+        map<string, int> p;
+        for (auto it : paths)
+        {
+            ++p[it[0]];
+            p[it[1]] += 2;
+        }
+        for (auto it : p)
+        {
+            if (it.second == 2)
+            {
+                return it.first;
+            }
+        }
+        return "Happy Coding";
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
