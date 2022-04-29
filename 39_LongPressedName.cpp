4c836db189500885ab75ed84fec554ba40389bed
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-29 21:25:59 +0530

    Wrong answer

---
+++ 39_LongPressedName.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/long-pressed-name/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isLongPressedName(string name, string typed)
+    {
+        if (name.length() > typed.length() || name[0] != typed[0]) {
+            return false ;
+        }
+        
+        int i = 0, j = 0;
+        while (i < name.length())
+        {
+            if (name[i] == typed[j])
+            {
+                ++j;
+                ++i;
+                continue ;
+            }
+            while (typed[j] == name[i-1] && j < typed.length()) {
+                ++j ;
+            }
+            if (typed[j] != name[i]) {
+                return false ;
+            }
+        }
+        if (j < typed.length()) {
+            while (j < typed.length()){
+                if (typed [j] != typed[i -1]) return false ;
+                ++j ;
+            }
+        }
+        return true ;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
